7816f09021e63ed60c601c1599d428629756f69f
Светлана <svardakov@yandex.ru>
2020-04-13 13:09:40 +0500

    commit home work 5

---
+++ main.py
@@ -0,0 +1,9 @@
+import divisor_master
+number = input('Введите натуральное число от 1 до 1000')
+number = int(number)
+print(divisor_master.simple_num(number))
+print(divisor_master.number_div(number))
+
+
+
+
