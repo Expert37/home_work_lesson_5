8c4ac4bec71774b655d90602d19d9a0ff84e5268
Светлана <svardakov@yandex.ru>
2020-04-17 16:14:27 +0500

    commit home work 6

--- test_divisor_master_1.py
+++ test_divisor_master_1.py
@@ -1,9 +1,12 @@
 import divisor_master_1
 
-# Тест 1 (правильно ли формируется список делитетелей числа 27?):
+# Тест 1 Правильно ли формируется список делитетелей числа 27? (не включая 1 и само число):
 
 def test_1():
-    assert divisor_master_1.number_div(27) == [1, 3, 9, 27]
+    assert divisor_master_1.number_div(27) == [3, 9]
 
+# Тест 2 Максимальным делитетем числа 36 является число 18?:
 
+def test_2():
+    assert max(divisor_master_1.number_div(36)) == 18
 
