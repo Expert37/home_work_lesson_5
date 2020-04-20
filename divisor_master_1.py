7052c56a4edbda8602389e9452a8266fd7485499
Светлана <svardakov@yandex.ru>
2020-04-20 12:46:30 +0500

    commit home work 6

--- divisor_master_1.py
+++ divisor_master_1.py
@@ -1,4 +1,4 @@
-
+# функция, которая возвращает список делителей числа
 def number_div(number):
     number_list = list(range(2, number))  # включая 1 и само число
     div_list = []   # создаем пустой список
@@ -6,6 +6,20 @@ def number_div(number):
         if number % i == 0: div_list.append(i)  # если в итерации находится число, на которое нацело делится number - то этот делитель добавляется в список div_list
     return div_list
 
+# функция, которая возвращает факториал числа
+def factorial(n):
+    res = 1
+    for i in range(1, n + 1):
+        res *= i
+    return res
+
+# функция, которая возвращает среднее арифметическое трех чисел:
+def average_func(x,y,z):
+    return (x+y+z)/3
+
+# функция, которая возвращает длину списка:
+def len_func(*args):
+    return len(args)
 
 
 
--- test_divisor_master_1.py
+++ test_divisor_master_1.py
@@ -10,3 +10,21 @@ def test_1():
 def test_2():
     assert max(divisor_master_1.number_div(36)) == 18
 
+# Тест 3 Факториал 5 = 120?:
+
+def test_3():
+    assert divisor_master_1.factorial(5) == 120
+
+# Тест 4 Среднее арифметическое чисел 10,20,30 = 20?:
+
+def test_4():
+    assert divisor_master_1.average_func(10,20,30) == 20
+
+# Тест 5 Проверка функции, которая возвращает длину списка длины списка:
+def test_5():
+    assert divisor_master_1.len_func('Sergey', 2, 5.1, 456) == 4
+
+
+
+
+
